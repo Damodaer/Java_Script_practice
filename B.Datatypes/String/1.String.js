34b038d046f7aba538cdc65c7f3358be7340f0b1
Damodaer <damodarsahuofficial94@gmail.com>
2023-04-26 12:04:24 +0530

    Java learning

---
+++ B.Datatypes/String/1.String.js
@@ -0,0 +1,189 @@
+const heading = document.getElementById("heading_1");
+
+let myname = "kanha sahu",
+  age = 28,
+  job = "software engineering";
+let a = 1,
+  b = "b",
+  c = "1";
+let str = "ashok sahu ";
+let str2 = "kanha sahu";
+let str3 = "ashok";
+let str4 = "ksah";
+let str5 = "ASHOK SAHU";
+let str6 = "ashok sahu";
+let num_str = "100.5";
+let num_str1 = "100.5xyz";
+let str_num = 100.5;
+// let name = 'ashok sahu'
+// let age = 28;
+// console.log({ myname, age ,job});
+console.log(`i am ${myname} and currently working on ${job}`); //template literal
+
+// let name1 = "i am single quote string";
+// let name2 = "i am dual quote string";
+// let name3 = `i am backtik string`;
+
+// let name1 = 'i a'm single qoute string';
+
+// let name1 = "i a'm single qoute string";
+
+// let name1 = "i a'm single qoute string";
+
+// let name1 = "i a"m single qoute string";
+
+// let bc = `i am a sting "i am " `;
+
+// let a = "a";
+// let b = "b";
+// let c = a + b; //string co ncatination
+
+// heading.textContent = typeof c//string
+// heading.textContent = typeof Number(c)//number
+// heading.textContent = typeof parseFloat(c)//number
+// heading.textContent = typeof parseInt(c)//number
+// heading.textContent = typeof +c//number
+
+// heading.textContent = a+b;
+// heading.textContent = a+c;
+// heading.textContent = a+ +c; //one of interview question
+// heading.textContent = a+Number(c);
+// heading.textContent = a+parseInt(c);
+// heading.textContent = a+parseFloat(c);
+
+//properties of string
+// let str = 'lorem ipsum bande materam';
+// heading.textContent = str.length //25
+// heading.textContent = str.charAt()//1
+// heading.textContent = str.charAt(0)//l
+// heading.textContent = str.charAt(1)//o
+// heading.textContent = str.charCodeAt()//108
+// heading.textContent = str.charCodeAt(22)//114
+// heading.textContent = str.charCodeAt(-22)//108
+
+// let unicodeArr = []
+// for (let i = 0 ; i<=str.length;i++){
+//   unicodeArr.push(str.charCodeAt(i))
+// }
+// heading.textContent = JSON.parse(JSON.stringify(unicodeArr))
+
+// heading.textContent = str.codePointAt(110) ?? 'undefined'
+// heading.textContent = undefined ?? 'undefined'
+// heading.textContent = null ?? 'undefined'
+// heading.textContent = '' ?? 'undefined'
+
+// let name1 = undefined,name2 = 'ashok';
+// let name1 = null,name2 = 'ashok';
+// let name1 = 'kanha',name2 = 'ashok';
+// let name1 = 1,name2 = 'ashok';
+// let name1 = 1.2,name2 = 'ashok';
+// let name1 = [],name2 = 'ashok';
+// let name1 = '',name2 = 'ashok';
+// let name1 = {} ,name2 = 'ashok';
+// let name1 = function(){} ,name2 = 'ashok';
+// heading.textContent = name1 ?? name2
+
+// heading.textContent = str.at(2)//depricated
+// heading.textContent = str.charAt(2)
+// heading.textContent = str.concat(str2)
+
+// heading.textContent = str2.endsWith('u')//true
+// heading.textContent = str2.endsWith('h')//false
+// heading.textContent = str2.endsWith('hu')//true
+// heading.textContent = str2.endsWith('bu')//true
+
+// heading.textContent = str2.startsWith('k')//true
+// heading.textContent = str2.startsWith('a')//false
+// heading.textContent = str2.startsWith('ka')//true
+
+// heading.textContent = str2.includes('k')//true
+// heading.textContent = str2.includes('a')//true
+// heading.textContent = str2.includes('n')//true
+// heading.textContent = str2.includes('x')//false
+
+// heading.textContent = str2.indexOf('k')//0
+// heading.textContent = str2.indexOf('a')//1
+// heading.textContent = str2.indexOf('n')//2
+// heading.textContent = str2.indexOf('x')//-1
+
+// heading.textContent = str2.lastIndexOf('a')//7
+// heading.textContent = str2.lastIndexOf('h')//7
+
+// heading.textContent = str[0]//a
+// heading.textContent = str.at(0)//a
+// heading.textContent = str.charAt(0)//a
+
+// heading.textContent = str.match(new RegExp('a'))
+// heading.textContent = typeof str.match(new RegExp('a'))
+// heading.textContent = typeof str.matchAll(new RegExp('a'))
+
+// heading.textContent = str.localeCompare(str3)//1
+// heading.textContent = str.localeCompare(str4)//-1
+
+// heading.textContent = str3.repeat(1)
+// heading.textContent = str3.repeat(2)
+// heading.textContent = str3.repeat(4)
+
+// heading.textContent = str.replace('ashok','kanha')
+// heading.textContent = str.replace('askok','kanha')
+
+// heading.textContent = str5.toLowerCase();
+// heading.textContent = str5.toUpperCase();
+
+// heading.textContent = str5.toLocaleLowerCase();
+// heading.textContent = str6.toLocaleUpperCase();
+
+// heading.textContent = str6.substring(0,5);
+// heading.textContent = str6.substring(6,11);
+
+// heading.textContent = str6.split().
+// heading.textContent = typeof str6.split()
+// heading.textContent = str6.split('')
+// heading.textContent = str6.split(' ')
+// heading.textContent = str6.split(',')
+// heading.textContent = str6.split('$')
+// heading.textContent = str6.split('a')
+// heading.textContent = str6.split('b')
+
+// heading.textContent = 'ashok       sahu'
+// heading.textContent = 'ashok       sahu'.length
+// heading.textContent = '     ashok sahu'.length
+// heading.textContent = '     ashok sahu'.trim().length
+// heading.textContent = '     ashok sahu       '.length
+// heading.textContent = '     ashok sahu       '.trim().length
+// heading.textContent = 'ashok sahu       '.length
+// heading.textContent = 'ashok sahu       '.trim().length
+
+// heading.textContent = '          ashok sahu'
+// heading.textContent = '             ashok sahu'.length
+// heading.textContent = '     ashok sahu'.trimStart()
+// heading.textContent = '         ashok sahu'.trimStart().length
+// heading.textContent = 'ashok sahu       '.length
+// heading.textContent = 'ashok sahu       '.trimStart().length
+// heading.textContent = 'ashok sahu       '.length
+// heading.textContent = 'ashok sahu       '.trimEnd().length
+
+// heading.textContent = typeof num_str
+// heading.textContent = typeof str_num
+// heading.textContent = typeof num_str1
+
+// heading.textContent = typeof toString(str_num)
+// heading.textContent = str_num.toString()
+// heading.textContent = typeof str_num.toString()
+
+let _variable = "string";
+// "abc".padStart(10); // "       abc"
+// "abc".padStart(10, "foo"); // "foofoofabc"
+// "abc".padStart(6, "123465"); // "123abc"
+// "abc".padStart(8, "0"); // "00000abc"
+// "abc".padStart(1); // "abc"
+
+// heading.textContent = _variable.padStart(10,'*')
+// _variable.padEnd(10,'*')
+// heading.textContent = _variable
+
+// heading.textContent = _variable.slice(1)
+// heading.textContent = _variable.slice(2)
+// heading.textContent = _variable.slice(2,4)
+_variable.slice(2, 4);
+heading.textContent = _variable;
---
+++ D.functions/basic.js
@@ -0,0 +1,152 @@
+// const item = document.getElementById("root");
+item.textContent = 'hello world'
+console.log({ item });
+
+// declaration of a function
+names()
+names()
+function names(){
+    console.log('this is function1')
+}
+function names(){
+    console.log('this is function2')
+}
+
+//calling a function
+// var fun1 = function () {
+//   console.log("calling fn1/");
+// };
+// let fun2 = function () {
+//   console.log("calling fn2/");
+// };
+// let fun3 = function () {
+//   return {};
+// };
+// console.log(typeof fun3());
+// console.log(
+//   typeof (function () {
+//     return "";
+//   })()
+// );
+
+// //ES6 es2015(arrow function)
+// const fun4 = () => {
+//   console.log("arrow function");
+// };
+// fun4();
+
+//? function with arguments
+// "use strict"
+// function arg1() {
+//   // console.log(typeof arguments)
+//   // console.log(typeof arguments.callee)
+//   // console.log(typeof arguments.length)
+//   // console.log(arguments)
+//   // console.log(arguments.callee)
+//   // console.log(arguments.length)
+//   arguments[5] = 5;
+//   console.log(arguments[0])
+//   console.log(arguments[1])
+//   console.log(arguments[2])
+//   console.log(arguments[3])
+//   console.log(arguments[4])
+//   console.log(arguments[5])
+//   console.log(arguments.length)
+
+//   //   for (let i = 0; i <= arguments.length; i++) console.log(arguments[i]);
+// }
+
+// arg1("", "a", 2, false);
+
+// function abc(name, age, job, company) {
+// //   console.log(name);
+// //   console.log(age);
+// //   console.log(job);
+// //   console.log(company);
+
+//   console.log({ name, age, job, company });
+// }
+
+// abc("ashok sahu", 28, "software engineer", "TCS");
+
+// function abc(name, age, job) {
+// //   console.log(name);
+// //   console.log(age);
+// //   console.log(job);
+// //   console.log(company);
+
+//   console.log({ name, age, job });
+// }
+
+// abc("ashok sahu", 28, "software engineer", "TCS");
+// abc("ashok sahu", 28);
+
+// default parameter
+// function abc(name, age=27, job='soft') {
+//   //   console.log(name);
+//   //   console.log(age);
+//   //   console.log(job);
+//   //   console.log(company);
+
+//   console.log({ name, age, job });
+// }
+
+// abc("ashok sahu", 28, "software engineer", "TCS");
+// abc("ashok sahu", 28);
+// abc("ashok sahu");
+
+// console.log('hello world')
+
+// let alexa = "hn ka re kan hela!";
+// let alexa = "hn ka re kan hela!";
+// console.log(alexa);
+// function getUser() {
+//   console.log(alexa);
+//   console.log(this)
+//   // console.log('hello world')
+// }
+
+// getUser();
+
+// function restParamsPre() {
+//   // console.log({ p1, p2, p3, p4, p5 }); //total arguments
+//   // console.log(arguments.length);
+
+//   // for (let i = 0; i <= arguments.length; i++) {
+//   //   console.log(arguments[i]);
+//   // }
+//   let arr = [];
+
+//   //? new loop for object and array { for..of }
+//   for (values of arguments) {
+//     // console.log(values);
+//     arr.push(values)
+//   }
+
+//   console.log(arr)
+// }
+// restParamsPre("ashok sahu", 28, "software developer", "TCS", false,'x','y','z');
+
+function restParams(p,p1,p2,...rest) {
+  // console.log({ p1, rest }); //total arguments
+  // console.log({ rest, arguments }); //total arguments
+  // console.log(arguments.length);
+
+  // console.log(typeof rest);
+  // console.log(typeof arguments);
+  // console.log(Array.isArray(rest));
+  // console.log(Array.isArray(arguments));
+
+  // console.log(rest.length); //all of the array method is work in rest
+  // console.log(arguments.length); //all of the array method is work in rest
+
+  // for (let i = 0; i <= arguments.length; i++) {
+  //   console.log(arguments[i]);
+  // }
+
+  //? new loop for object and array { for..of }
+  for (values of rest) {
+    console.log(values);
+  }
+}
+restParams("ashok sahu", 28, "software developer", "TCS", false);
